910f7aaf0d54c32091cfff530662ec3f77c7f292
Big Guy <104549927+BigManThonga@users.noreply.github.com>
2024-12-25 17:02:05 -0500

    Add files via upload

---
+++ SYSTEM/ShlOSCore.py
@@ -0,0 +1,15 @@
+# ShellOS Version: 1.0
+# Core Version: 1.1
+# ShellOS Shell Version: 2.0
+
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+GUI = os.path.join(script_dir, "Graphical_Shell/GraphicalShell.py")
+
+# Check if the ShellOS script exists
+if os.path.exists(GUI):
+    os.system(f'python "{GUI}"')
+else:
+    print(f"Error: {GUI} not found.")
